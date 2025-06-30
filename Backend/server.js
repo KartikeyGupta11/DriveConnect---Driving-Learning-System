b4372ba5288a875d577c725e1868c414199a0065
Kartikey <g.kartikey.1104@gmail.com>
2025-06-30 15:01:58 +0530

    Cloud DB is initialized

--- Backend/server.js
+++ Backend/server.js
@@ -19,6 +19,10 @@ const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(cors());
+// app.use(cors({
+//     origin: 'https://your-netlify-site.netlify.app', // your frontend domain
+//     credentials: true
+//   }));
 app.use(express.json());
 
 // ✅ Static file serving for uploaded images/docs
